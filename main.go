6c601f76e76ffbb35609bd3e289b0e5023609cc4
php1301 <19520854@gm.uit.edu.vn>
2021-04-05 21:47:40 +0700

    logger added

--- main.go
+++ main.go
@@ -1,4 +1,14 @@
 package main
+
+import (
+	"fmt"
+	"mercee-be/middleware"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 func main(){
-	
+	r := gin.Default()
+	middleware.SetupLogOutput()
 }
