d7d4d15dd2f02edd443c789512a5f0e647d87688
php1301 <19520854@gm.uit.edu.vn>
2021-04-06 21:30:49 +0700

    Connect to MySQL

--- main.go
+++ main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"mercee-be/config"
 	"mercee-be/middleware"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
-func main(){
+
+func main() {
 	r := gin.Default()
 	middleware.SetupLogOutput()
+
+	db, err := gorm.Open(mysql.Open(config.DbUrl(config.BuildDBConfig())), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println("Status", err)
+	}
+	db.AutoMigrate()
 }
